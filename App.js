72d540f77871da5f5a682af0c79282f63b091d90
Sam Johnson <s_johnso@yahoo.com>
2023-11-21 19:07:17 -0600

    setup

--- App.js
+++ App.js
@@ -1,16 +1,24 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, SafeAreaView } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+    <NavigationContainer>
+    <SafeAreaView style={styles.safeArea}>
+      <View style={styles.container}>
+      <Text> App.js to start working on your app!</Text>
       <StatusBar style="auto" />
     </View>
+    </SafeAreaView>
+    </NavigationContainer>
   );
 }
 
 const styles = StyleSheet.create({
+  safeArea: {
+    flex: 2
+  },
   container: {
     flex: 1,
     backgroundColor: '#fff',
