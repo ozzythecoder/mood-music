0f12e4e89acd4afe2676f40dd4bae83804809eba
Sam Johnson <s_johnso@yahoo.com>
2023-11-26 15:38:16 -0600

    added bottom nav bar navigation with songs screen

--- App.tsx
+++ App.tsx
@@ -1,16 +1,34 @@
-import { StatusBar } from 'expo-status-bar';
-import { SafeAreaView, StyleSheet, Text, View } from 'react-native';
-import { NavigationContainer } from '@react-navigation/native';
+import React from "react";
+import { StatusBar } from "expo-status-bar";
+import { SafeAreaView, StyleSheet, Text, View } from "react-native";
+import { NavigationContainer } from "@react-navigation/native";
+import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
+import SongsScreen from "./screens/SongsScreen";
+import Ionicons from '@expo/vector-icons/Ionicons';
+
+const Tab = createBottomTabNavigator();
 
 export default function App() {
   return (
     <NavigationContainer>
-      <SafeAreaView style={styles.safeArea}>
-        <View style={styles.container}>
-          <Text>Howdy!!!</Text>
-          <StatusBar style="auto" />
-        </View>
-      </SafeAreaView >
+      <Tab.Navigator
+        screenOptions={({ route }) => ({
+          tabBarIcon: ({ focused, color, size }) => {
+            let iconName;
+
+            if (route.name === 'Songs') {
+              iconName = focused
+                    ? 'musical-notes'
+                    : 'musical-notes-outline'
+                }
+                return <Ionicons name={iconName} size={size} color={color} />;
+          },
+          tabBarActiveTintColor: 'tomato',
+          tabBarInactiveTintColor: 'gray',
+        })}
+      >
+        <Tab.Screen name="Songs" component={SongsScreen} />
+      </Tab.Navigator>
     </NavigationContainer>
   );
 }
@@ -21,8 +39,8 @@ const styles = StyleSheet.create({
   },
   container: {
     flex: 1,
-    backgroundColor: '#ccc',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#ccc",
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
