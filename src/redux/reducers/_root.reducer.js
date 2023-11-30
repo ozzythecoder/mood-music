6a4f3cc21c340dda731ac63e62cd139c94a0b449
Sam Johnson <s_johnso@yahoo.com>
2023-11-30 13:43:11 -0600

    added redux and sagas to try to use store as a way to add moods to songs

---
+++ src/redux/reducers/_root.reducer.js
@@ -0,0 +1,9 @@
+import { combineReducers } from 'redux';
+
+// rootReducer is the primary reducer for our entire project
+// This is imported in index.js as rootSaga'
+const rootReducer = combineReducers({
+
+});
+
+export default rootReducer;
