dce9c4a4baf1c592d09dea759da0f46befc8fa25
Sam Johnson <s_johnso@yahoo.com>
2023-12-01 10:02:11 -0600

    added boilerplate language for a get songs saga

--- src/redux/reducers/song.reducer.ts
+++ src/redux/reducers/song.reducer.ts
@@ -1,9 +1,11 @@
 const clickedSong = (state = [], action) => {
-    switch (action.type) {
-      case 'SET_CLICKED_SONG':
-        return action.payload;
-      default:
-        return state;
-    }
-  };
-  export default clickedSong;
+  switch (action.type) {
+    case "SET_CLICKED_SONG":
+      return action.payload;
+    case "SET_SONGS":
+      return action.payload;
+    default:
+      return state;
+  }
+};
+export default clickedSong;
