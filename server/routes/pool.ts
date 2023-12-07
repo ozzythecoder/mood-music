7a8f48cfb204eab76ce49b5c1492ba238ad85a56
Sam Johnson <s_johnso@yahoo.com>
2023-12-07 12:45:12 -0600

    working on node connection to mongo

---
+++ server/routes/pool.ts
@@ -0,0 +1,22 @@
+
+const pg = require('pg');
+let pool;
+
+if (process.env.DATABASE_URL) {
+    pool = new pg.Pool({
+        connectionString: process.env.DATABASE_URL,
+        ssl: {
+            rejectUnauthorized: false
+        }
+    });
+}
+
+else {
+    pool = new pg.Pool({
+        host: 'localhost',
+        port: 5432,
+        database: 'movement_phraser',   
+    });
+}
+
+module.exports = pool;
