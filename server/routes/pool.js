730f949ba8ca2c9e391f49d560fbaa7493a7d948
Sam Johnson <s_johnso@yahoo.com>
2023-12-10 18:24:46 -0600

    cleaned code

--- server/routes/pool.js
+++ server/routes/pool.js
@@ -1,8 +1,7 @@
 const {MongoClient} = require('mongodb');
 
     const uri = 
-        "mongodb+srv://sjohnamson:nBnJJBi1TF4FWa5I@moodmusiccluster.9p8vq5c.mongodb.net/?retryWrites=true&w=majority";
-
+process.env.MONGODB_URI
     const pool = new MongoClient(uri);
 
 module.exports = pool; 
