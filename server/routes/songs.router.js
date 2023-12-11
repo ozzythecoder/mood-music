18b3b363dbc8c006012ec73a39beb57943c36e73
Sam Johnson <s_johnso@yahoo.com>
2023-12-11 13:02:09 -0600

    added get songs to songs.router

--- server/routes/songs.router.js
+++ server/routes/songs.router.js
@@ -3,6 +3,28 @@ const router = express.Router();
 const client = require("./pool");
 // const {rejectUnauthenticated} = require('../modules/authentication-middleware');
 
+router.get("/", async (req, res) => {
+  console.log("in song get router");
+
+      // Connect to the MongoDB cluster
+      const connection = await client.connect();
+
+  try {
+    await connection.query('BEGIN');
+    // Make the appropriate DB calls
+    // GET full list of songs from DB
+    const result = await getSongs(client);
+    await connection.query('COMMIT');
+    res.send(result.rows)
+  } catch (error) {
+    console.log(`Transaction Error - Rolling back new account`, error);
+    res.sendStatus(500);
+  } finally {
+    // Close the connection to the MongoDB cluster
+    await client.close();
+  }
+});
+
 router.post("/", async (req, res) => {
   console.log("in song router:", req.body.moods);
   // const userCurrent = req.user.clicked_song;
@@ -21,15 +43,33 @@ console.log('newSong:', newSong)
     // Make the appropriate DB calls
     // Create a single new listing
     await addSong(client, newSong);
+  } catch (error) {
+    console.log(`Transaction Error - Rolling back new account`, error);
+    res.sendStatus(500);
   } finally {
     // Close the connection to the MongoDB cluster
     await client.close();
   }
 });
 
+async function getSongs(client) {
+  const cursor = client
+    .db("mood-music")
+    .collection("songs")
+    .find();
+
+  const results = await cursor.toArray();
+
+    console.log(`Found songs:`);
+    results.forEach((result, i) => {
+        console.log(result.title);
+    });
+}
+
 async function addSong(client, newSong) {
   const result = await client.db("mood-music").collection("songs").insertOne(newSong);
   console.log(`New song added with the following id: ${result.insertedId}`);
 }
+
 
 module.exports = router;
