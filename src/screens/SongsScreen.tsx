a62aa43b0b1e0ad92b97519552fcef3cabeac54a
Sam Johnson <s_johnso@yahoo.com>
2023-12-14 13:25:05 -0600

    song search styling

--- src/screens/SongsScreen.tsx
+++ src/screens/SongsScreen.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { SafeAreaView, StyleSheet} from "react-native";
+import { SafeAreaView, StyleSheet, View} from "react-native";
 import { useState } from "react";
 import SongList from "../components/SongFlatList";
 import SongSearch from "../components/SongSearchBar";
@@ -21,27 +21,5 @@ export default function Songs({ navigation }: {navigation: any}) {
 const styles = StyleSheet.create({
   safeArea: {
     flex: 1,
-  },
-  container: {
-    flex: 1,
-    backgroundColor: "#ccc",
-    alignItems: "center",
-    justifyContent: "center",
-  },
-  header: {
-    backgroundColor: "pink",
-    height: 40,
-    paddingHorizontal: 5,
-  },
-  text: {
-    color: "black",
-    fontWeight: "bold",
-  },
-  subtext: {
-    color: "black",
-  },
-  song: {
-    marginBottom: 5,
-    padding: 3,
   },
 });
