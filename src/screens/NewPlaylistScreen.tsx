14869b2e493d886e231974d5b098dfd628e4fbfc
Sam Johnson <s_johnso@yahoo.com>
2023-12-31 17:00:46 -0600

    add client side save playlist button - needs saga and route

--- src/screens/NewPlaylistScreen.tsx
+++ src/screens/NewPlaylistScreen.tsx
@@ -1,39 +1,47 @@
-import React from "react";
+import React, { useState } from "react";
 import { useSelector } from "react-redux";
-import { SafeAreaView, StyleSheet} from "react-native";
+import { SafeAreaView, StyleSheet } from "react-native";
 import NewPlaylist from "../components/NewPlaylistFlatList";
 import CreatePlaylistButton from "../components/CreatePlaylistButton";
+import PlaylistTitle from "../components/PlaylistTitleField";
+import SavePlaylistButton from "../components/SavePlaylistButton";
 
-type ClickedMoodType = { 
+type ClickedMoodType = {
   clickedMood: {
-  _id: string; 
-  moodName: string; 
-  color: string
-  } 
-}
+    _id: string;
+    moodName: string;
+    color: string;
+  };
+};
 
-export default function NewPlaylistScreen({ navigation }: {navigation: any}) {
-  const clickedMood = useSelector((store: ClickedMoodType) => store.clickedMood);
+export default function NewPlaylistScreen({ navigation }: { navigation: any }) {
+  const clickedMood = useSelector(
+    (store: ClickedMoodType) => store.clickedMood
+  );
+  const [playlistTitle, setPlaylistTitle] = useState("");
 
   return (
     <SafeAreaView style={styles.safeArea}>
-{/* playback control buttons eventually? */}
+      {/* playback control buttons eventually? */}
 
-{/* flatlist with the playlist 
+      {/* flatlist with the playlist 
     needs re-regenerate switch*/}
-<NewPlaylist />
-{/* re-generate button */}
-<CreatePlaylistButton clickedMood = {clickedMood} />
-{/* title your playlist field  */}
-{/* save button */}
-
+      <NewPlaylist />
+      {/* re-generate button */}
+      <CreatePlaylistButton clickedMood={clickedMood} />
+      {/* title your playlist field  */}
+      <PlaylistTitle
+        playlistTitle={playlistTitle}
+        setPlaylistTitle={setPlaylistTitle}
+      />
+      {/* save button */}
+      <SavePlaylistButton playlistTitle={playlistTitle} />
     </SafeAreaView>
   );
 }
 
 const styles = StyleSheet.create({
-    safeArea: {
-      flex: 1,
-    },
-  });
-  
+  safeArea: {
+    flex: 1,
+  },
+});
