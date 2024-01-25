fc2ab685b11d74575e0350456de6957294472df4
August McAllister <august.chm@gmail.com>
2024-01-25 13:20:09 -0600

    Components dir now uses imported definitions
    Also left some notes where it felt appropriate

--- src/components/SongMoodFlatList.tsx
+++ src/components/SongMoodFlatList.tsx
@@ -1,35 +1,34 @@
 import React from "react";
 import { StyleSheet, View, FlatList } from "react-native";
-import { SongType } from "./SongFlatList";
+import type { Song } from "@src/definitions";
 
-
-const SongMoodList = ({ song }: { song: SongType }) => {
-  return (
-    <View style={styles.moods}>
-      <FlatList
-        horizontal={true}
-        data={song.moodFull}
-        keyExtractor={(item) => item.moodName}
-        renderItem={(data) => (
-          <View
-            style={[styles.moodBox, { backgroundColor: data.item.color }]}
-          />
-        )}
-      />
-      </View>
-  );
+const SongMoodList = ({ song }: { song: Song }) => {
+    return (
+        <View style={styles.moods}>
+            <FlatList
+                horizontal={true}
+                data={song.moodFull}
+                keyExtractor={(item) => item.moodName}
+                renderItem={(data) => (
+                    <View
+                        style={[styles.moodBox, { backgroundColor: data.item.color }]}
+                    />
+                )}
+            />
+        </View>
+    );
 };
 
 export default SongMoodList;
 
 const styles = StyleSheet.create({
-  moods: {
-marginBottom: 2,
-  },
-  moodBox: {
-    height: 10,
-    width: 10,
-    margin: 1,
-    borderRadius: 5,
-  },
+    moods: {
+        marginBottom: 2,
+    },
+    moodBox: {
+        height: 10,
+        width: 10,
+        margin: 1,
+        borderRadius: 5,
+    },
 });
