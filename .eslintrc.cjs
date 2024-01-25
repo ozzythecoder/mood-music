90fe2bf6d9cd7d84536ba5d8c53663994cc0c1a6
August McAllister <august.chm@gmail.com>
2024-01-24 22:39:49 -0600

    Further eslint rule adjustment

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -36,17 +36,20 @@ module.exports = {
         sourceType: "module",
     },
     rules: {
+        "@stylistic/max-len": ["warn", 100],
         "@stylistic/quotes": ["warn", "double", { avoidEscape: true }],
         "@stylistic/semi": ["warn", "always"],
         "@stylistic/indent": ["warn", 4],
         "@stylistic/jsx-indent": ["warn", 4],
-        "@stylistic/member-delimiter-style": ["warn", { multiline: { delimiter: "semi" } }],
+        "@stylistic/jsx-indent-props": ["warn", 4],
+        "@stylistic/member-delimiter-style": [
+            "warn",
+            {
+                multiline: {
+                    delimiter: "semi",
+                },
+            }],
         "@stylistic/brace-style": ["warn", "1tbs"],
-        // "@typescript-eslint/explicit-function-return-type": 0,
-        // "@typescript-eslint/indent": 0,
-        // "@typescript-eslint/space-before-function-paren": 0,
-        // "@typescript-eslint/keyword-spacing": 0,
-        // "@typescript-eslint/comma-dangle": 0,
         "react-native/no-unused-styles": 1,
         "react-native/split-platform-components": 2,
         "react-native/no-inline-styles": 0,
