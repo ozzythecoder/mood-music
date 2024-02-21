a239819e309c9be6fca9438e013be16503221a31
August McAllister <august.chm@gmail.com>
2024-02-21 11:42:21 -0600

    Linted

--- server/routes/userlogin.router.js
+++ server/routes/userlogin.router.js
@@ -1,36 +1,34 @@
-const express = require('express');
+const express = require("express");
 const {
-  rejectUnauthenticated,
-} = require('../modules/authentication-middleware');
-const encryptLib = require('../modules/encryption');
-const client = require('./pool');
-const userStrategy = require('../strategies/user.strategy');
+    rejectUnauthenticated,
+} = require("../modules/authentication-middleware");
+const encryptLib = require("../modules/encryption");
+const client = require("./pool");
+const userStrategy = require("../strategies/user.strategy");
 
 const router = express.Router();
 
-router.get('/', (req, res) => {
-  console.log('hello');
-  res.send(req.user);
+router.get("/", (req, res) => {
+    console.log("hello");
+    res.send(req.user);
 });
 
-router.post('/register', async (req, res, next) => {
+router.post("/register", async (req, res, next) => {
     const password = encryptLib.encryptPassword(req.query.password);
     const doc = {
         username: req.query.username,
         password: password,
-    }
-    try{
+    };
+    try {
         await client.connect();
         const result = await client.db("mood-music").collection("users").insertOne(doc);
 
         console.log(result);
         res.sendStatus(201);
-    } catch(error) {
-        console.log("ERROR: ", error)
+    } catch (error) {
+        console.log("ERROR: ", error);
         res.sendStatus(500);
     }
-
-    
 
     // const queryText = `INSERT INTO "users" (username, password)
     //     VALUES ($1, $2) RETURNING id`;
@@ -43,19 +41,19 @@ router.post('/register', async (req, res, next) => {
     //     });
 });
 
-router.post('/login', userStrategy.authenticate('local'), (req, res) => {
-    //console.log("LOGGING USER IN........", req);
+router.post("/login", userStrategy.authenticate("local"), (req, res) => {
+    // console.log("LOGGING USER IN........", req);
 
     res.sendStatus(200);
 });
 
-router.post('/logout', (req, res) => {
-  req.logout();
-  res.sendStatus(200);
+router.post("/logout", (req, res) => {
+    req.logout();
+    res.sendStatus(200);
 });
 
-async function registerUser(client, doc){
-    
+async function registerUser(client, doc) {
+
 }
 
 module.exports = router;
