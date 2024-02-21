ddd4652105ca7edf05295af59e0b027acfdc5bcb
August McAllister <august.chm@gmail.com>
2024-02-21 13:33:45 -0600

    Deleted js files

--- server/routes/moods.router.js
+++ server/routes/moods.router.js
@@ -1,40 +1,38 @@
 const express = require("express");
 const router = express.Router();
-const client = require("./pool");
+const { client } = require("../db");
 // const {rejectUnauthenticated} = require('../modules/authentication-middleware');
 
 router.get("/", async (req, res) => {
-  console.log("in mood get router");
+    console.log("in mood get router");
 
-  try {
-      // Connect to the MongoDB cluster
-      await client.connect();
+    try {
+        // Connect to the MongoDB cluster
+        await client.connect();
 
-    // Make the appropriate DB calls
-    // GET full list of songs from DB
-    const result = await getMoods(client);
-    console.log('result:', result)
-    res.send(result)
-  } catch (error) {
-    console.log(`Transaction Error - Rolling back new account`, error);
-    res.sendStatus(500);
-  } finally {
+        // Make the appropriate DB calls
+        // GET full list of songs from DB
+        const result = await getMoods(client);
+        console.log("result:", result);
+        res.send(result);
+    } catch (error) {
+        console.log("Transaction Error - Rolling back new account", error);
+        res.sendStatus(500);
+    } finally {
     // Close the connection to the MongoDB cluster
-    client.close();
-  }
+        client.close();
+    }
 });
 
 async function getMoods(client) {
-  const cursor = client
-    .db("mood-music")
-    .collection("moods")
-    .find();
+    const cursor = client
+        .db("mood-music")
+        .collection("moods")
+        .find();
 
-  const results = await cursor.toArray();
+    const results = await cursor.toArray();
 
-return results
+    return results;
 }
-
-
 
 module.exports = router;
