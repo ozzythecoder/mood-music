1862b7a059d2e5763b65bce0e1fe6a6a1eb5e992
August McAllister <august.chm@gmail.com>
2024-03-07 12:34:53 -0600

    Created barrel file for routes dir

---
+++ server/routes/index.ts
@@ -0,0 +1,8 @@
+import dbPlaylistRoute from "./dbplaylists.router";
+import playlistsRoute from "./playlist.router";
+import moodsRoute from "./moods.router";
+import songsRoute from "./songs.router";
+import spotifyRoute from "./spotify.router";
+import userLoginRoute from "./userlogin.router";
+
+export { dbPlaylistRoute, playlistsRoute, moodsRoute, songsRoute, spotifyRoute, userLoginRoute };
